79a39510052b96c6dc0b2b6e97291f8680ecf495
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2018-04-21 20:20:10 +0100

    Fixing the svg style

--- monitor_mobile/App.js
+++ monitor_mobile/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/HomeScreen';
+// require('./app.css');
 
 
 export default class App extends React.Component {
--- monitor_mobile/src/HomeScreen.js
+++ monitor_mobile/src/HomeScreen.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import PubNubReact from 'pubnub-react';
 import PubNub from 'pubnub';
- 
-
+import { Constants, Svg } from 'expo';
  
 const subscribeKey = "sub-c-24d83964-40ef-11e8-a2e8-d2288b7dcaaf";
 const publishKey = "pub-c-deda0dd4-b711-466b-8bbb-c12e7e0e43e0";
@@ -22,52 +20,115 @@ export default class HomeScreen extends React.Component {
   }
   
   state = {
-    current_temperature: ''
+    current_temperature: '00.00'
   }
 
   componentDidMount() {
     this.pubnub.addListener({
-      message: (m) => this.setState({
-        current_temperature : Number(m.message.temperature).toFixed(2) 
-      }),
-      
-      status: function(s) {
-          var affectedChannelGroups = s.affectedChannelGroups;
-          var affectedChannels = s.affectedChannels;
-          var category = s.category;
-          var operation = s.operation;
-      }
+      message: (m) => this._handle_temperature_update(m),
+      status: (s) => { console.log('STATUS:',s) }
     });
 
-    this.pubnub.subscribe({
-      channels: [channel_name],
-      withPresence: true
-    });
+    this.pubnub.subscribe({ channels: [channel_name], withPresence: true });
   }
   
   componentWillUnmount() {
-    this.pubnub.unsubscribe({ channels: [channel_name] });
+    this.pubnub.removeListener(this.pubnub)
   }
   
-  _handle_temperature_update = (temp) => {
-    this.setState({ current_temperature: temp })
-    console.log('About to upate temperature', temp);
+  _handle_temperature_update = (m) => {
+    this.setState({
+      current_temperature : Number(m.message.temperature).toFixed(2) 
+    })
+  }
+
+  _handle_draw_arc = () => {
+    const angle = this.state.current_temperature/100 * 360;
+    return describeArc(200, 200, 138, 0, parseInt(angle));
   }
 
   render() {
+    const d  = this._handle_draw_arc()
     return (
       <View style={styles.container}>
+        <View style={styles.userDetailContainer}>
+          <Text style={styles.name}>Durotola Samuel Oluwaseun</Text>
+          <Text style={styles.dept}>Dept. of Physics</Text>
+          <Text style={styles.marticno}>14PY1013</Text>
+        </View>
         <Text>TEMPERATURE {this.state.current_temperature}</Text>
+        
+        <View style={styles.containment}>
+          <Svg height={400} width={400}>
+            <Svg.Circle cx={200} cy={200} r={150} stroke-width={10} stroke="#fff"  fill="#fff" />
+            <Text style={styles.tempStyle}>
+              {`${this.state.current_temperature} C`}
+            </Text>
+            <Svg.Path d={d} fill="none" stroke="#867DF2" stroke-width="15" />
+            <Svg.Circle cx={200} cy={200} r={120} stroke-width={1} stroke="#e74c3c"  fill="#e74c3c" />
+          </Svg>
+        </View>
       </View>
     );
   }
 }
 
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
+
+  var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+  var d = [
+    "M", start.x, start.y, "A", radius, radius, 0, arcSweep, 0, end.x, end.y
+  ].join(" ");
+  return d;       
+}
+
 const styles = StyleSheet.create({
+  userDetailContainer: {
+    backgroundColor: '#3A3B5D',
+  },
+  name: {
+    fontSize: 30,
+    color: "#FFF"
+  },
+  dept: {
+    fontSize: 30,
+    color: "#FFF"
+  },
+  marticno: {
+    fontSize: 30,
+    color: "#FFF"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containment: {
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#3A3B5D',
+    bottom: 0
+  },
+  tempStyle: {
+    position: 'absolute',
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+    backgroundColor: '#FFF'
+  }
 });
