9f81833c19455d4758d29674acffed6dea6c5f21
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2018-04-22 04:26:14 +0100

    Added the indicator LEDs

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 import smbus2
 import bme280
 import time
+import RPi.GPIO as GPIO
 
 # Initialize the pubnub service
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.exceptions import PubNubException
 from pubnub.pubnub import PubNub
 
+# Set up the indicator LEDs
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT) # Pin for when running
+GPIO.setup(20,GPIO.OUT) # Pin for error
+
+# Set up Pubnub configuration
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-24d83964-40ef-11e8-a2e8-d2288b7dcaaf"
 pnconfig.publish_key = "pub-c-deda0dd4-b711-466b-8bbb-c12e7e0e43e0"
@@ -43,10 +51,14 @@ def push_to_pubnub(data):
 			"pressure": data.pressure,
 			"humidity": data.humidity
 	    }).sync()
+        GPIO.output(20,GPIO.LOW)
     except PubNubException as e:
         handle_exception(e)
-
+        GPIO.output(20,GPIO.HIGH)
 
 while True:
     push_to_pubnub(read_temp())
-    time.sleep(1)
+    time.sleep(0.5)
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(18,GPIO.LOW)
